5a261e95ddd1f0f5bd7c4527c102c07949ecbc6e
stooged <stooged@users.noreply.github.com>
2018-06-24 12:48:22 +0800

    add win make

--- build.sh
+++ build.sh
@@ -6,8 +6,10 @@ popd
 pushd DB_SG_Backup
 make
 popd
+mkdir -p bin
 rm -f bin/DB_SG_Backup.bin
 cp DB_SG_Backup/DB_SG_Backup.bin bin/DB_SG_Backup.bin
+mkdir -p html_payload
 tool/bin2js bin/DB_SG_Backup.bin > html_payload/payload.js
 sed "s/###/$(cat html_payload/payload.js)/" exploit.template > html_payload/DB_SG_Backup.html
 rm -f DB_SG_Backup/DB_SG_Backup.bin
