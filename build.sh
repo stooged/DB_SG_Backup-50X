facf4c63fd5ba28ebf6bdefcab86f021a3a0f274
stooged <stooged@users.noreply.github.com>
2018-07-01 12:50:54 +0800

    v2

--- build.sh
+++ build.sh
@@ -11,6 +11,13 @@ rm -f bin/DB_SG_Backup.bin
 cp DB_SG_Backup/DB_SG_Backup.bin bin/DB_SG_Backup.bin
 mkdir -p html_payload
 tool/bin2js bin/DB_SG_Backup.bin > html_payload/payload.js
-sed "s/###/$(cat html_payload/payload.js)/" exploit.template > html_payload/DB_SG_Backup.html
+FILESIZE=$(stat -c%s "bin/DB_SG_Backup.bin")
+PNAME=$"DB_SG_Backup"
+cp exploit.template html_payload/DB_SG_Backup.html
+sed -i -f - html_payload/DB_SG_Backup.html << EOF
+s/#NAME#/$PNAME/g
+s/#BUF#/$FILESIZE/g
+s/#PAY#/$(cat html_payload/payload.js)/g
+EOF
 rm -f DB_SG_Backup/DB_SG_Backup.bin
 rm -f html_payload/payload.js
